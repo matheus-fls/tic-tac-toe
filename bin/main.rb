8f85aefcaffda371fdd6c48a02f4723c2e06716e
oscarnava <oscarnava@gmail.com>
2019-08-22 13:08:12 -0500

    Imported banner code and adapted it

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,18 @@
 require_relative '../lib/game.rb'
+require_relative '../lib/banner.rb'
 
 puts "START..."
 
+alphabet = Alphabet.new
+alphabet.read
+
+puts alphabet.to_banner "O|X|3"
+puts "###################################"
+puts alphabet.to_banner "4|5|9"
+puts "###################################"
+puts alphabet.to_banner "7|8|3"
+
+=begin
 game = Game.new(0)
 
 puts game
@@ -9,7 +20,7 @@ puts game
 until game.winner || game.tie? do
     print "\nMove: "
     move = gets.chomp.to_i
-    unless game.play(move - 1) 
+    unless game.play(move - 1)
       puts "Invalid move! Try again"
     end
     puts "#{game}\n"
@@ -18,5 +29,6 @@ end
 if game.winner
   puts "#{game.winner} wins!"
 else
-  puts "It's a tie!" 
+  puts "It's a tie!"
 end
+=end
---
+++ lib/banner.rb
@@ -0,0 +1,111 @@
+
+# Holds the scan lines for one character
+class Character
+    
+    def initialize 
+        @scan_lines = []    # Strings to represent the character
+    end
+    
+    # Receives one line for the character representation 
+    # and pushes it to the scan lines
+    def push(line)
+        @scan_lines << line
+    end
+    
+    # Gets the scan line at 'index' for the character
+    def get(index)
+        @scan_lines[index]
+    end
+    
+    # Just return this character as an string (for debug purposes)
+    def to_s
+        @scan_lines.join("\n")
+    end
+    
+end
+
+# Holds a series of scan lines for zero or more characters
+class Banner
+    
+    # Takes the height of each line and an optional initial group of scan lines
+    def initialize(height, lines = Array.new(height,""))
+        @height = height
+        @lines  = lines
+    end
+    
+    # Receives an instance of Char and appends the scan lines from it to the
+    # current scan lines for this banner. Returns a new instance of Banner
+    # holding the new scanlines
+    def add(char)
+        Banner.new(@height, @lines.map.with_index { |line,idx| line + char.get(idx) })
+    end
+   
+    # Returns all the appended scan lines separated by a new line.
+    def to_s
+        @lines.join("\n")
+    end
+    
+end
+
+# Holds the alphabet characters, reads the input and generates banners
+class Alphabet
+
+  @@ALPHABET = [
+  #  123456712345671234567123456712345671234567123456712345671234567123456712345671234567
+    "                                                                                #   ", # 1
+    "  ###   #   #    #     ###   #####     #   #####    ##   #####   ###    ###     #   ", # 2
+    " #   #  #   #   ##    #   #     #     ##   #       #         #  #   #  #   #    #   ", # 3
+    " #   #   # #     #        #    #     # #   ####   #         #   #   #  #   #    #   ", # 4
+    " #   #    #      #       #      #   #  #       #  ####     #     ###    ####    #   ", # 5
+    " #   #   # #     #      #        #  #####      #  #   #    #    #   #      #    #   ", # 6
+    " #   #  #   #    #     #     #   #     #   #   #  #   #    #    #   #     #     #   ", # 7
+    "  ###   #   #   ###   #####   ###      #    ###    ###     #     ###    ##      #   ", # 8
+    "                                                                                #   "  # 9
+  ]
+
+  def initialize(width = 7, height = 9)
+    @width  = width
+    @height = height
+    @chars  = {}
+    "OX123456789|?".split("").map { |ch| @chars[ch] = Character.new }
+  end
+
+  # Reads the letters representation, splits it and initialize each character scan lines
+  def read
+    @@ALPHABET.each do |row|
+      @chars.each do | _ , char |
+        char.push row[0..@width - 1]
+        row = row[@width..-1]
+      end
+    end
+  end
+
+  # Takes some string and creates a banner by appending each character scan lines
+  # to a banner. Each character appended creates a new banner.
+  def to_banner(text)
+    banner = Banner.new(@height)
+    text.upcase.split("").each do |ch|
+      banner = banner.add(@chars[@chars[ch] ? ch : '?'])
+    end
+    banner
+  end
+
+end
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+=begin 
+width  = gets.to_i
+height = gets.to_i
+text   = gets.chomp
+
+alphabet = Alphabet.new width, height
+alphabet.read
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
+puts alphabet.to_banner 'OOX'
+puts alphabet.to_banner 'OXO'
+puts alphabet.to_banner 'XXX'
+=end
