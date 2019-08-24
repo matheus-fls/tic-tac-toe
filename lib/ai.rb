ec2b146d935f9d77e9e57fbfefadb72dea4cb665
oscarnava <oscarnava@gmail.com>
2019-08-24 17:13:54 -0500

    Cleanup code for AI

--- lib/ai.rb
+++ lib/ai.rb
@@ -4,10 +4,11 @@ require_relative './player.rb'
 
 class AIPlayer < Player
   def fetch_play(current_game)
-    current_game.store_state
-    move = current_game.valid_moves.length == 1 ? current_game.valid_moves.first : calc_montecarlo(current_game)
-    current_game.restore_state
-    move
+    if current_game.valid_moves.length == 1
+      current_game.valid_moves.first
+    else
+      calc_montecarlo(current_game)
+    end
   end
 
   def play_random_game(game, first_move)
@@ -30,12 +31,14 @@ class AIPlayer < Player
   end
 
   def calc_montecarlo(game)
+    game.store_state
+
     print "\nThinking..."
     values = Array.new(10, 0)
     counts = Array.new(10, 0)
 
     total = 0
-    eta = Time.now + 0.1
+    eta = Time.now + 0.5
     while Time.now < eta
 
       game.restore_state
@@ -43,12 +46,7 @@ class AIPlayer < Player
                game.valid_moves.sample
              else
                mul = 2.0 * Math.log(total)
-               # puts "\n\nTotal: #{total}, mul: #{mul}\n" if (total % 500).zero?
-               game.valid_moves.max_by do |mov|
-                 values[mov].to_f / counts[mov] + Math.sqrt(mul / counts[mov])
-                 # puts "Factors for #{mov}: #{a} + #{b} = #{score}" if (total % 500).zero?
-               end
-               # puts "#{best}" if (total % 500).zero?
+               game.valid_moves.max_by { |mov| values[mov].to_f / counts[mov] + Math.sqrt(mul / counts[mov]) }
              end
 
       values[move] += play_random_game(game, move)
@@ -57,12 +55,8 @@ class AIPlayer < Player
       print '.' if (total % 500).zero?
     end
 
-    # puts "Number of simulations: #{total}"
-
     game.restore_state
-    game.valid_moves.max_by do |mov|
-      # puts "Heuristic for #{mov}: #{values[mov]}/#{counts[mov]} = #{score}"
-      values[mov].to_f / counts[mov]
-    end
+    mul = 2.0 * Math.log(total)
+    game.valid_moves.max_by { |mov| values[mov].to_f / counts[mov] - Math.sqrt(mul / counts[mov]) }
   end
 end
